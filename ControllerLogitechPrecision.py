b844940f761f27a62163c9dc73335e7d94a0c4bc
rchoekstra <rchoekstra@gmail.com>
2019-11-24 06:59:52 +0000

    Added comments to ControllerLogitiechPrecision.py and created helper_functions.py

--- ControllerLogitechPrecision.py
+++ ControllerLogitechPrecision.py
@@ -5,9 +5,14 @@ import asyncio
 class ControllerLogitechPrecision:
     def __init__(self, callback, input_device):
         self.callback = callback
+
+        # Open a linux input device from which input events can be read
         self.device = evdev.InputDevice(input_device)
         print(self.device)
 
+        # Initialize dict that stores the controller button states. Te keys corresponds to the
+        # event codes from the controller. The keys 2-5 don't correspond the a event code but
+        # are added for easier use.
         self.btns = {  0: {'name': 'X', 'value': 128}
                     ,  1: {'name': 'Y', 'value': 128}
                     ,  2: {'name': 'LEFT', 'value': 0}
@@ -26,16 +31,27 @@ class ControllerLogitechPrecision:
                     ,297: {'name': 'START', 'value': 0}
         }
 
+        # Add additional dict that translates human friendly name to event code.
         self.btn_keys = {self.btns[x]['name']:x for x in self.btns}
 
 
     async def get_events(self):
+        """Get events from controller
+
+        Each button press and release trigger an event (device.async_read_loop). The events are
+        translated to a state for each button in the btns dict.
+
+        When the event data is stored al callback function is called, which enables further 
+        processing of the controller input.
+        """
         tmp = {}
         async for event in self.device.async_read_loop():
             if event.type in (evdev.ecodes.EV_KEY, evdev.ecodes.EV_ABS):
+
+                # Save button state to btns dict.
                 self.btns[event.code]['value'] = event.value
 
-                # Recode left/right
+                # Recode X-axis into left/right
                 if event.code==0:
                     if self.btns[0]['value']==1:    # LEFT
                         self.btns[2]['value']=1
@@ -47,7 +63,7 @@ class ControllerLogitechPrecision:
                         self.btns[2]['value']=0
                         self.btns[3]['value']=1
 
-                # Recode up/down
+                # Recode Y-axis up/down
                 elif event.code==1:
                     if self.btns[1]['value']==1:    # UP
                         self.btns[4]['value']=1
@@ -59,9 +75,7 @@ class ControllerLogitechPrecision:
                         self.btns[4]['value']=0
                         self.btns[5]['value']=1
 
-                #for btn in self.btns:
-                #    print(self.btns[btn]['name'],': ', self.btns[btn]['value'], sep='')
-                #print("")
+                # Call the callback function
                 self.callback("input.gamepad",self.btns) # self.btns: within ControllerLogitechPrecision of PiBot?
 
         await asyncio.sleep(1/125) # Polling rate of 125Hz
