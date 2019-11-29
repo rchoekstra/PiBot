008ce209ad20eb3c207b444fc85be6dc92884810
rchoekstra <rchoekstra@gmail.com>
2019-11-29 10:12:19 +0000

    Swapped in_min and in_max in clamp()

--- helper_functions.py
+++ helper_functions.py
@@ -4,4 +4,4 @@ def map_range(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def clamp(x, out_min, out_max):
-    return max(min(x,out_min),out_max)
+    return max(min(x,out_max),out_min)
